983d11814046157f3f7da25c534ff13ea0222a23
Akbarshokh <bumur@DESKTOP>
2023-12-04 20:11:17 +0200

    changing layout screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,28 @@
 import { Stack } from "expo-router";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import {
+    AmaticSC_400Regular,
+    AmaticSC_700Bold,
+  } from "@expo-google-fonts/amatic-sc";
+  import { useEffect } from "react";
+  import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
+  const [fontLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError]);
+
+  if (!fontLoaded && !fontError) {
+    return null;
+  }
   return (
     <Stack screenOptions={{}}>
       <Stack.Screen name="index" options={{ title: "Devember" }} />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,12 @@
-import { useEffect } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import DataListItem from "../components/core/DayListItem";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import * as SplashScreen from "expo-splash-screen";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
 
 SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function HomeScreen() {
-  const [fontLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontLoaded, fontError]);
-
-  if (!fontLoaded && !fontError) {
-    return null;
-  }
 
   return (
     <View style={styles.container}>
