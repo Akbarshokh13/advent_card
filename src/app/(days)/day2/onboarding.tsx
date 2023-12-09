869a5dbd923a2d307769b647a5bd63fcc5938dc2
Akbarshokh <bumur@DESKTOP>
2023-12-09 11:09:32 +0200

    updating the onboarding screen, adding screen indicator

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -1,4 +1,5 @@
-import { Link, Stack } from "expo-router";
+import { useState } from "react";
+import { Link, Stack, router } from "expo-router";
 import {
   Text,
   View,
@@ -8,57 +9,95 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { GestureDetector, Gesture, Directions} from "react-native-gesture-handler";
 
 const onboardingSteps = [
   {
-    icon: "people-arrows",
-    title: "Track every transaction",
+    icon: "snowflake-4",
+    title: "Welcome to #DEVember",
     description:
       " Monitor your spending and contribution, ensuring every penny aligns with your familys aspirations",
   },
   {
-    icon: "people-arrows",
-    title: "Track every transaction",
-    description:
-      " Monitor your spending and contribution, ensuring every penny aligns with your familys aspirations",
+    icon: "cast-for-education",
+    title: "Learn and grow together",
+    description: "Learn by building 24 projects with React Native and Expo",
   },
   {
     icon: "people-arrows",
-    title: "Track every transaction",
+    title: "Education for children",
     description:
-      " Monitor your spending and contribution, ensuring every penny aligns with your familys aspirations",
+      "Contribute to the fundraser ''Education for children'' to help Save the Children",
   },
 ];
 
 const onboarding = () => {
+  const [screenIndex, setScreenIndex] = useState(0);
+
+  const data = onboardingSteps[screenIndex];
+
+  const onContinue = () => {
+    const isLastScreen = screenIndex === onboardingSteps.length - 1;
+    if (isLastScreen) {
+      endOnboarding();
+    } else {
+      setScreenIndex(screenIndex + 1);
+    }
+  };
+
+  const endOnboarding = () => {
+    setScreenIndex(0);
+    router.back();
+  };
+
+  const fling = Gesture.Fling(); 
+  // fling.direction(Directions.RIGHT | Directions.LEFT);
+
   return (
     <SafeAreaView style={styles.page}>
+      <View style={styles.stepIndicatorContainer}>
+        {onboardingSteps.map((step, index) => (
+          <View
+            style={[
+              styles.stepIndicator,
+              { backgroundColor: index === screenIndex ? "#CEF202" : "gray" },
+            ]}
+          ></View>
+        ))}
+      </View>
       <Stack.Screen options={{ headerShown: false }} />
-      <View style={styles.pageContent}>
-        <FontAwesome5
-          style={styles.image}
-          name="people-arrows"
-          size={100}
-          color="#CEF202"
-        />
+      <StatusBar style="light" />
 
-        <View style={styles.footer}>
-          <Text style={styles.title}> Track every transaction</Text>
-          <Text style={styles.description}>
-            Monitor your spending and contribution, ensuring every penny aligns
-            with your family's aspirations
-          </Text>
-          <View style={styles.buttonsRow}>
-            <Text style={styles.buttonText}>Skip</Text>
-            <Link href="/day2/onboarding2" asChild>
+      <GestureDetector gesture={fling}>
+        <View style={styles.pageContent}>
+          <Fontisto
+            name="snowflake-4"
+            size={100}
+            style={styles.image}
+            color="#CEF202"
+          />
+
+          <View style={styles.footer}>
+            <Text style={styles.title}>{data.title}</Text>
+            <Text style={styles.description}>{data.description}</Text>
+            <View style={styles.buttonsRow}>
+              <Pressable>
+                <Text onPress={endOnboarding} style={styles.buttonText}>
+                  Skip
+                </Text>
+              </Pressable>
               <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Continue</Text>
+                <Text onPress={onContinue} style={styles.buttonText}>
+                  Continue
+                </Text>
               </TouchableOpacity>
-            </Link>
+            </View>
           </View>
         </View>
-      </View>
+      </GestureDetector>
     </SafeAreaView>
   );
 };
@@ -72,6 +111,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#15141A",
   },
+  stepIndicatorContainer: {
+    flexDirection: "row",
+    gap: 5,
+    padding: 60,
+  },
+
+  stepIndicator: {
+    flex: 1,
+    width: 100,
+    height: 3,
+    backgroundColor: "gray",
+    margin: 5,
+    borderRadius: 10,
+  },
+
   pageContent: {
     padding: 20,
     flex: 1,
@@ -93,6 +147,7 @@ const styles = StyleSheet.create({
   image: {
     alignSelf: "center",
     margin: 20,
+    marginTop: 50,
   },
   footer: {
     marginTop: "auto",
